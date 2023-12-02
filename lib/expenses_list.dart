78074a542038a5fd6c7c387b3207830f323b0fc1
shakeelahammedabdulla <4pa20cs103@pace.edu.in>
2023-12-02 12:17:17 +0530

    initial commit

---
+++ lib/expenses_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Expenses_List extends StatelessWidget {
+  const Expenses_List({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
