8f71c808f9025c0b6827d5c937a3b7d0b6696bfd
shakeelahammedabdulla <4pa20cs103@pace.edu.in>
2023-12-05 22:34:45 +0530

    widget files are sorted

--- lib/widgets/expenses_list/expenses_jtem.dart
+++ lib/widgets/expenses_list/expenses_jtem.dart
@@ -1,14 +1,71 @@
+// import 'package:expense_tracker/models/expense.dart';
+// import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:flutter/material.dart';
+
+// class ExpenseItem extends StatelessWidget {
+//   const ExpenseItem(this.expense, {super.key});
+
+//   final Expense expense;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(
+//         horizontal: 20,
+//         vertical: 16,
+//       ),
+//       child: Column(
+//         children: [
+//           Text(expense.title),
+//           const SizedBox(height: 4),
+//           Row(
+//             children: [
+//               Text('\$${expense.amount.toStringAsFixed(2)}'),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem(this.expense, {super.key});
+  const ExpenseItem(this.expense, {Key? key}) : super(key: key);
 
-final Expense expense;
+  final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: Text(expense.title));
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      child: Column(
+        children: [
+          Text(expense.title),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              Text('\$${expense.amount.toStringAsFixed(2)}'),
+              const Spacer(),
+              Row(
+                children: [
+                  const Icon(Icons.alarm),
+                  const SizedBox(width: 8),
+                  Text(expense.date.toString()),
+
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
